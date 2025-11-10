73f1e3acf8e74c999ca8735d1a70fc4f84dbef5c
Patrick Foltz <historian@racerhistory.com>
2025-11-09 21:51:51 -0600

    Add opponent fields (mascot, current, short, abbr) and update basketball stats to use team_nickname

--- src/Model/Entity/Opponent.php
+++ src/Model/Entity/Opponent.php
@@ -10,8 +10,12 @@ use Cake\ORM\Entity;
  *
  * @property int $id
  * @property string|null $opponent_name
- * @property int|null $place_id
+ * @property string|null $opponent_mascot Team mascot or nickname
  * @property int|null $opponent_current Reference to current opponent (self-reference)
+ * @property string|null $opponent_short Shortened name (max 30 chars)
+ * @property string|null $opponent_abbr Scorebug abbreviation (max 6 chars)
+ * @property int|null $place_id
+ * @property \App\Model\Entity\Place|null $place
  * @property \App\Model\Entity\Opponent|null $current_opponent
  * @property \Cake\I18n\DateTime|null $created_at
  * @property \Cake\I18n\DateTime|null $updated_at
