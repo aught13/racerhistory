a32c6dc2933e1c7a2267225cd9ceb97eced8c0e4
Patrick Foltz <historian@racerhistory.com>
2026-06-09 18:13:59 -0500

    my dumbass forgot to indent

--- js/__mocks__/@hotwired/turbo.js
+++ js/__mocks__/@hotwired/turbo.js
@@ -5,11 +5,11 @@
  * can be tested under Jest without installing the full package.
  */
 export default {
-  start() {},
-  visit() {},
-  clearCache() {},
-  navigator: {},
-  session: {},
-  connectStreamSource() {},
-  disconnectStreamSource() {},
+    start() {},
+    visit() {},
+    clearCache() {},
+    navigator: {},
+    session: {},
+    connectStreamSource() {},
+    disconnectStreamSource() {},
 };
